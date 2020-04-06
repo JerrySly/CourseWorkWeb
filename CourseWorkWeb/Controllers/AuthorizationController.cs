5bf26afe493ef7afff20ff6bcccf18d3f3cb4cf4
JerrySly <52193061+JerrySly@users.noreply.github.com>
2020-04-07 00:58:49 +0300

    Изменена вкладка авторизации и регистрации, добавлены соответствующие этим двум страницам методы у контроллера Authorization. Добавлено подключение к базе данных и настроена регистрация(добавляет юзера в базу)

---
+++ CourseWorkWeb/Controllers/AuthorizationController.cs
@@ -0,0 +1,41 @@
+﻿using CourseWorkWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace CourseWorkWeb.Controllers
+{
+    public class AuthorizationController:Controller
+    {
+        private ApplicationContext _db;
+        public AuthorizationController(ApplicationContext context)
+        {
+            _db = context;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+       /*public  async Task<IActionResult> Index()
+        {
+            return View( await _db.Users.ToListAsync());
+        }*/
+     
+        public IActionResult Registred()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async  Task<IActionResult> Registred(User user)
+        {
+            _db.Users.Add(user);
+            await _db.SaveChangesAsync();
+            return  RedirectToAction("Index");
+
+        }
+    }
+}
